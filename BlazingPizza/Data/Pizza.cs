f9fd3c20b0f0f64250f3340877b27352e378c154
defdalila <defdalila@outlook.com>
2025-08-15 08:16:10 -0300

    add  muita coisa

---
+++ BlazingPizza/Data/Pizza.cs
@@ -0,0 +1,38 @@
+﻿namespace BlazingPizza.Data
+{
+    /// <summary>
+    /// Defines the <see cref="Pizza" />
+    /// </summary>
+    public class Pizza
+    {
+        /// <summary>
+        /// Gets or sets the PizzaId
+        /// </summary>
+        public int PizzaId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Price
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether Vegetarian
+        /// </summary>
+        public bool Vegetarian { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether Vegan
+        /// </summary>
+        public bool Vegan { get; set; }
+    }
+}
---
+++ BlazingPizza/Model/OrderWithStatus.cs
@@ -0,0 +1,40 @@
+﻿namespace BlazingPizza.Model
+{
+    using System;
+
+    public class OrderWithStatus
+    {
+        public static readonly TimeSpan PreparationDuration = TimeSpan.FromSeconds(10);
+
+        public static readonly TimeSpan DeliveryDuration = TimeSpan.FromMinutes(1); // Unrealistic, but more interesting to watch
+
+        public Order Order { get; set; }
+
+        public string StatusText { get; set; }
+
+        public bool IsDelivered => StatusText == "Delivered";
+
+        public static OrderWithStatus FromOrder(Order order)
+        {
+            // To simulate a real backend process, we fake status updates based on the amount
+            // of time since the order was placed
+            string statusText;
+            var dispatchTime = order.CreatedTime.Add(PreparationDuration);
+
+            if (DateTime.Now < dispatchTime)
+            {
+                statusText = "Preparing";
+            }
+            else if (DateTime.Now < dispatchTime + DeliveryDuration)
+            {
+                statusText = "Out for delivery";
+            }
+            else
+            {
+                statusText = "Delivered";
+            }
+
+            return new OrderWithStatus { Order = order, StatusText = statusText };
+        }
+    }
+}
